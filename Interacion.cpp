f4773eddd9ba01169f8342cf9acb7b0334830d3f
miguelechave11 <miguelechave11@gmail.com>
2017-06-01 19:38:13 +0200

    Add files via upload

---
+++ Interacion.cpp
@@ -0,0 +1,23 @@
+/* Interacion
+    Miguel Echave
+    26/5/2017
+*/
+
+
+#include <iostream>
+
+
+using namespace std;
+
+int main()
+{
+    int i=0;
+
+    while (i<122)
+    {
+        cout<<i<<"Voy a practicar programacion"<<endl;
+        i++;
+    }
+
+    return 0;
+}
---
+++ Saltos.cpp
@@ -0,0 +1,25 @@
+/* Saltos
+    Miguel Echave
+    25/5/2017
+*/
+
+#include <iostream>
+
+
+using namespace std;
+
+int main()
+{
+    int i=0;
+
+volver:if(i<122)
+    {
+        cout<<i<<"Voy a practicar programacion"<< endl;
+        i++;
+        goto volver;
+
+    }
+
+
+    return 0;
+}
